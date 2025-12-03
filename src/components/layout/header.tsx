38944fdc171dcb37147e0172eeb82abd8901862b
Pardi Pardi <pardi.jhr@gmail.com>
2025-12-03 16:32:42 +0000

    Buat warna yang cerah untuk tombol kembali

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -23,7 +23,7 @@ export function Header({ title }: { title: string }) {
     <header className="sticky top-0 z-30 flex h-16 items-center gap-4 border-b bg-background/80 px-4 backdrop-blur-sm md:px-6">
       <SidebarTrigger className="md:hidden" />
       {!isHomePage && (
-        <Button asChild variant="outline" size="icon" className="h-8 w-8">
+        <Button asChild variant="default" size="icon" className="h-8 w-8">
             <Link href="/">
                 <ArrowLeft className="h-4 w-4" />
                 <span className="sr-only">Kembali ke Menu Utama</span>
