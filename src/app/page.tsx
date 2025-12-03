bfeb493d33b5483b177892e5a7b3d6642ef0598f
Pardi Pardi <pardi.jhr@gmail.com>
2025-12-03 15:53:50 +0000

    tulisan "KAKEKNGAMIN" ubah menjadi "KAKEK NGAMIN"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <main className="flex-1 p-4 md:p-6 space-y-6">
         <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2">
             <h1 className="font-headline text-2xl font-extrabold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-blue-500 sm:text-3xl">
-            ARISAN KELUARGA BESAR Alm. KAKEKNGAMIN
+            ARISAN KELUARGA BESAR Alm. KAKEK NGAMIN
             </h1>
             <RealTimeClock />
         </div>
