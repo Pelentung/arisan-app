02d76d8878fe6245c482956076969db298340a01
Pardi Pardi <pardi.jhr@gmail.com>
2025-12-06 02:05:22 +0000

    error saat di deploy ke vercel dan muncul ini : Vulnerable version of Ne

--- next.config.ts
+++ next.config.ts
@@ -57,6 +57,10 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  webpack: (config) => {
+    config.externals = [...config.externals, 'canvas', 'jsdom'];
+    return config;
+  },
 };
 
 export default withPWA(nextConfig);
