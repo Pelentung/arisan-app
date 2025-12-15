d49a894a62b48ae74a62435381f703d3ed25f63c
Pardi Pardi <pardi.jhr@gmail.com>
2025-12-15 04:50:23 +0000

    masukan kode iklan ini : <script type="text/javascript">   atOptions = {

--- src/components/dashboard/advertisement-section.tsx
+++ src/components/dashboard/advertisement-section.tsx
@@ -5,43 +5,33 @@ import { Card, CardContent } from '@/components/ui/card';
 
 export function AdvertisementSection() {
     const adContainerRef = useRef<HTMLDivElement>(null);
-    const adsterraId = process.env.NEXT_PUBLIC_ADSTERRA_AD_ID;
 
     useEffect(() => {
-        if (adContainerRef.current && !adContainerRef.current.querySelector('script')) {
-            const script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.innerHTML = `
+        const adContainer = adContainerRef.current;
+        if (adContainer && !adContainer.hasChildNodes()) {
+            const optionsScript = document.createElement('script');
+            optionsScript.type = 'text/javascript';
+            optionsScript.innerHTML = `
                 atOptions = {
-                    'key' : '${adsterraId}',
-                    'format' : 'native',
-                    'height' : 250,
-                    'width' : 300,
+                    'key' : '4a8bcfd01f80e85866ef6313080077d4',
+                    'format' : 'iframe',
+                    'height' : 60,
+                    'width' : 468,
                     'params' : {}
                 };
             `;
-            adContainerRef.current.appendChild(script);
+            adContainer.appendChild(optionsScript);
 
-            const adScript = document.createElement('script');
-            adScript.type = 'text/javascript';
-            adScript.src = '//www.profitabledisplaynetwork.com/e5/a8/19/e5a8192329391d575727937402f153b9.js';
-            adContainerRef.current.appendChild(adScript);
+            const invokeScript = document.createElement('script');
+            invokeScript.type = 'text/javascript';
+            invokeScript.src = 'https://www.highperformanceformat.com/4a8bcfd01f80e85866ef6313080077d4/invoke.js';
+            adContainer.appendChild(invokeScript);
         }
-    }, [adsterraId]);
-    
-    if (!adsterraId) {
-        return (
-            <Card>
-                <CardContent className="p-4 text-center text-muted-foreground">
-                    ID Iklan Adsterra belum dikonfigurasi.
-                </CardContent>
-            </Card>
-        );
-    }
+    }, []);
     
   return (
     <Card>
-      <CardContent ref={adContainerRef} className="p-2 flex justify-center items-center">
+      <CardContent ref={adContainerRef} className="p-2 flex justify-center items-center min-h-[60px]">
         {/* Adsterra ad will be injected here */}
       </CardContent>
     </Card>
